c12ee01a10bf5b886ed044604cfae9f8a849c8ff
AkramMondal2 <abcmondalxyz@gmail.com>
2025-11-17 08:35:57 +0530

    add session and logout function

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,8 +1,9 @@
 import userSchema from "../models/userSchema.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import { verifyMail } from "../emailVerify/verifyMail.js";
+import sessionSchema from "../models/sessionSchema.js";
 
 export const register = async (req, res) => {
   try {
@@ -27,14 +28,14 @@ export const register = async (req, res) => {
       password: hashedPassword,
     });
 
-    const token = jwt.sign({id:user._id},process.env.secretKey,{
-      expiresIn: "5m"
-    })
+    const token = jwt.sign({ id: user._id }, process.env.secretKey, {
+      expiresIn: "5m",
+    });
 
-    verifyMail(token,email)
+    verifyMail(token, email);
 
-    user.token = token
-    await user.save()
+    user.token = token;
+    await user.save();
 
     return res.status(200).json({
       sucess: true,
@@ -66,6 +67,12 @@ export const login = async (req, res) => {
         message: "Invalide Password",
       });
     } else if (passwordCheck && user.verified === true) {
+      await sessionSchema.findOneAndDelete({
+        userId: user._id,
+      });
+      await sessionSchema.create({
+        userId: user._id,
+      });
       const accessToken = jwt.sign(
         {
           id: user._id,
@@ -102,6 +109,31 @@ export const login = async (req, res) => {
       });
     }
   } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
+
+export const logOut = async (req, res) => {
+  try {
+    const exiesting = await sessionSchema.findOne({ userId: req.userId });
+    if (exiesting) {
+      await sessionSchema.findOneAndDelete({ userId: req.userId });
+      await userSchema.findByIdAndUpdate(req.userId, { isLoggedIn: false });
+      return res.status(200).json({
+        sucess: true,
+        message: "Session end sucessfuly",
+      });
+    } else {
+      return res.status(400).json({
+        sucess: false,
+        message: "User does not have any session",
+      });
+    }
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({
       sucess: false,
       message: error.message,
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { login, register } from "../controllers/userControler.js";
+import { login, logOut, register } from "../controllers/userControler.js";
 import { verification } from "../middleware/verifyToken.js";
+import { hasToken } from "../middleware/hasToken.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.get("/verifyToken", verification);
 userRouter.post("/login", login);
+userRouter.delete("/logout",hasToken,logOut);
 
 export default userRouter;
